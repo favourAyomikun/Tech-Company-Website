ca39fdfedb9c61cf265aa04fa7ddbf2e45f652d8
Oshoaro Favour <oshoarofavour@gmail.com>
2024-10-09 14:48:18 +0100

    created the sign up page and included some validations

--- app/page.jsx
+++ app/page.jsx
@@ -67,7 +67,7 @@ const Home = () => {
 
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-md w-96">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm md:max-w-md mx-auto">
         <h2 className="text-2xl font-bold mb-6 text-center text-gray-600">
           Sign Up
         </h2>
